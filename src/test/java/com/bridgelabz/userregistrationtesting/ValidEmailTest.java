de98ccf98495280ff1d0b2297e783ecb07dc6f64
Akash Singh <akashsingh6280@gmail.com>
2021-09-11 04:21:31 +0530

    ADDED: parameterised test to verify list of email

---
+++ src/test/java/com/bridgelabz/userregistrationtesting/ValidEmailTest.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.userregistrationtesting;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ValidEmailTest{
+	 String email;
+	 boolean expectedOutput;
+
+	public ValidEmailTest(String email, boolean expectedOutput) {
+		this.email = email;
+		this.expectedOutput = expectedOutput;
+	}
+	@Test
+	public void givenEmailAsVariable_ShouldReturnAsPerTheParameteriszedResult() 
+	{
+		UserRegistration Validator = new UserRegistration();
+		boolean result=Validator.validateEmail(email);
+		Assert.assertEquals(expectedOutput,result);
+	}
+	@Parameterized.Parameters
+	public static Collection input() {
+		return Arrays.asList(new Object[][] { { "abc@yahoo.com",true }, { "abc-100@yahoo.com",true }, { "abc.100@yahoo.com",true },
+					{ "abc111@abc.com",true }, { "abc-100@abc.net",true }, { "abc.100@abc.com.au",true }, { "abc@1.com",true },
+					{ "abc@gmail.com.com",true }, { "abc+100@gmail.com",true }, { "abc",false }, { "abc@.com.my",false }, { "abc123@gmail.a",false },
+					{ "abc123@.com",false }, { "abc123@.com.com",false }, { ".abc@abc.com",false }, { "abc()*@gmail.com",false }, { "abc@%*.com",false },
+					{ "abc..2002@gmail.com",false }, { "abc.@gmail.com",false }, { "abcabc@gmail.com",true }, { "abc@gmail.com.1a",false },
+					{ "abc@gmail.com.aa.au",false } });
+		}
+
+}
