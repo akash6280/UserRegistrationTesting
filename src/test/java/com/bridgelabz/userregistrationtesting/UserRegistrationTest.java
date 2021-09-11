dac4012e98b71a272dd9ffba3c80b0fce9f64688
Akash Singh <akashsingh6280@gmail.com>
2021-09-11 06:16:06 +0530

    ADDED: test case to handle  null value of password

--- src/test/java/com/bridgelabz/userregistrationtesting/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationtesting/UserRegistrationTest.java
@@ -252,6 +252,16 @@ public class UserRegistrationTest {
 			Assert.assertEquals(UserRegistrationException.ExceptionType.ENTERED_EMPTY, e.type);
 		}
 	}
+	@Test
+	public void givenPassword_WhenNull_ShouldThrowException() throws UserRegistrationException {
+		UserRegistration validator = new UserRegistration();	
+		try {
+			validator.validatePassword(null);
+		}
+		catch(UserRegistrationException e) {
+			Assert.assertEquals(UserRegistrationException.ExceptionType.ENTERED_NULL, e.type);
+		}
+	}
 	
 	
 }
